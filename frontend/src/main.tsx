9ba7f63a593260fe761b002bf559f68a6478819a
Krip Topongkasem <krip.t@ku.th>
2026-09-19 21:50:07 +0700

    feat: add complete shopping cart system (#17)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CartProvider } from './cart/CartContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode><CartProvider><App /></CartProvider></React.StrictMode>,
 );
